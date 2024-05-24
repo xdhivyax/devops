4630d531a45c61fa0a7f98837d30bac7726da815
dhivya <dhivya123@gmail.com>
2024-05-24 19:42:02 +0530

    update install.sh

--- Ansible/install.sh
+++ Ansible/install.sh
@@ -2,4 +2,10 @@ sudo apt update
 sudo apt install openjdk-11-jdk -y
 sudo apt install docker.io -y
 sudo apt install python3 
-
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian/jenkins.io-2023.key
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install jenkins -y
