3ce8abdb80b8051cc7d2db0dceaaa03d92c27761
Dhivya <113448671+xdhivyax@users.noreply.github.com>
2024-07-17 18:42:06 +0530

    Add files via upload

---
+++ terraform/aws/userdata-vpc.tf
@@ -0,0 +1,137 @@
+terraform {
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            # optional
+            version = "~> 3.0"
+       }
+    }
+}
+
+# Configuring provider
+provider "aws" {
+    region = "us-east-2"
+    access_key = ""
+    secret_key = ""
+}
+
+# Creating a VPC
+resource "aws_vpc" "my-vpc" {
+    cidr_block = "10.0.0.0/16"
+}
+
+# Create an Internet Gateway
+resource "aws_internet_gateway" "my-ig" {
+    vpc_id = aws_vpc.my-vpc.id
+    tags = {
+        Name = "internet-gateway1"
+    }
+}
+
+# Setting up the route table
+resource "aws_route_table" "test-rt" {
+    vpc_id = aws_vpc.my-vpc.id
+    route {
+        # pointing to the internet
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.my-ig.id
+    }
+    route {
+        ipv6_cidr_block = "::/0"
+        gateway_id = aws_internet_gateway.my-ig.id
+    }
+    tags = {
+        Name = "rt1"
+    }
+}
+
+# Setting up the subnet
+resource "aws_subnet" "public-subnet" {
+    vpc_id = aws_vpc.my-vpc.id
+    cidr_block = "10.0.1.0/24"
+    availability_zone = "us-east-2b"
+    tags = {
+        Name = "public-subnet"
+    }
+}
+
+# Associating the subnet with the route table
+resource "aws_route_table_association" "test-rt-sub-assoc" {
+    subnet_id = aws_subnet.public-subnet.id
+    route_table_id = aws_route_table.test-rt.id
+}
+
+# Creating a Security Group
+resource "aws_security_group" "test-sg" {
+    name = "test-sg"
+    description = "Enable web traffic for the project"
+    vpc_id = aws_vpc.my-vpc.id
+    ingress {
+        description = "HTTPS traffic"
+        from_port = 443
+        to_port = 443
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        description = "HTTP traffic"
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        description = "SSH port"
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
+    tags = {
+        Name = "test-sg1"
+    }
+}
+
+# Creating a new network interface
+resource "aws_network_interface" "test-ni" {
+    subnet_id = aws_subnet.public-subnet.id
+    private_ips = ["10.0.1.10"]
+    security_groups = [aws_security_group.test-sg.id]
+}
+
+# Attaching an elastic IP to the network interface
+resource "aws_eip" "test-eip" {
+    vpc = true
+    network_interface = aws_network_interface.test-ni.id
+    associate_with_private_ip = "10.0.1.10"
+}
+
+# Creating an Ubuntu EC2 instance
+resource "aws_instance" "test-instance" {
+    ami = ""
+    instance_type = "t2.micro"
+    availability_zone = "us-east-2b"
+    key_name = "keypair"
+    network_interface {
+        device_index = 0
+        network_interface_id = aws_network_interface.test-ni.id
+    }
+    user_data = <<-EOF
+            #!/bin/bash
+            sudo apt update -y
+            sudo apt install nginx -y
+            sudo systemctl start nginx
+            sudo systemctl enable nginx
+    EOF
+    tags = {
+        Name = "test-instance"
+    }
+}   
+
